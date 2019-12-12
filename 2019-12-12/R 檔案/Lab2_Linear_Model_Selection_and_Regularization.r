6c60c2b07ff3e02aff0005c24e2f246c43d42958
alanchua92 <alan_chua921223@hotmail.com>
2019-12-12 10:38:01 +0800

    機器學習 code

--- 2019-12-12/R 檔案/Lab2_Linear_Model_Selection_and_Regularization.r
+++ 2019-12-12/R 檔案/Lab2_Linear_Model_Selection_and_Regularization.r
@@ -2,9 +2,9 @@
 library(ISLR) #安裝需要的package
 
 data(Hitters) #取出資料
-head(Hitters)
+head(Hitters) #看資料的前幾筆
 
-?Hitters
+?Hitters #可以詳細了解這筆資料的每個變數
 
 names(Hitters) #看資料的欄位名稱
 
@@ -19,25 +19,30 @@ sum(is.na(Hitters )) #檢查是否還有缺失值尚未刪除
 # install.packages("leaps")
 library(leaps) #安裝需要的package 
 
-regfit.full=regsubsets(Salary~.,data=Hitters ,nvmax =19)
+regfit.full=regsubsets(Salary~.,data=Hitters ,nvmax =8)
 
-reg.summary=summary(regfit.full)
+summary(regfit.full)
 
-reg.summary$adjr2
+regfit.full_1=regsubsets(Salary~.,data=Hitters ,nvmax =19)
+summary(regfit.full_1)
+
+reg.summary_1=summary(regfit.full_1)
+
+reg.summary_1$adjr2
 
 par(mfrow =c(2,2))
 
-plot(reg.summary$rss ,xlab=" Number of Variables ",ylab=" RSS",type="l")
-points(which.min(reg.summary$rss), reg.summary$rss[which.min(reg.summary$rss)], col ="red",cex =2, pch =20)
+plot(reg.summary_1$rss ,xlab=" Number of Variables ",ylab=" RSS",type="l")
+points(which.min(reg.summary_1$rss), reg.summary_1$rss[which.min(reg.summary_1$rss)], col ="red",cex =2, pch =20)
 
-plot(reg.summary$adjr2 ,xlab =" Number of Variables ", ylab=" Adjusted RSq",type="l")
-points(which.max(reg.summary$adjr2), reg.summary$adjr2[which.max(reg.summary$adjr2)], col ="red",cex =2, pch =20)
+plot(reg.summary_1$adjr2 ,xlab =" Number of Variables ", ylab=" Adjusted RSq",type="l")
+points(which.max(reg.summary_1$adjr2), reg.summary_1$adjr2[which.max(reg.summary_1$adjr2)], col ="red",cex =2, pch =20)
 
-plot(reg.summary$cp ,xlab ="Number of Variables",ylab="Cp",type='l')
-points(which.min(reg.summary$cp), reg.summary$cp[which.min(reg.summary$cp)], col ="red",cex =2, pch =20)
+plot(reg.summary_1$cp ,xlab ="Number of Variables",ylab="Cp",type='l')
+points(which.min(reg.summary_1$cp), reg.summary_1$cp[which.min(reg.summary_1$cp)], col ="red",cex =2, pch =20)
 
-plot(reg.summary$bic ,xlab="Number of Variables",ylab=" BIC",type='l')
-points(which.min(reg.summary$bic), reg.summary$bic[which.min(reg.summary$bic)], col =" red",cex =2, pch =20)
+plot(reg.summary_1$bic ,xlab="Number of Variables",ylab=" BIC",type='l')
+points(which.min(reg.summary_1$bic), reg.summary_1$bic[which.min(reg.summary_1$bic)], col =" red",cex =2, pch =20)
 
 regfit.fwd=regsubsets(Salary~.,data=Hitters, nvmax =19, method ="forward")
 
@@ -72,7 +77,7 @@ lasso.coef
 
 x_new=x[,names(lasso.coef[lasso.coef!=0])[-1]]
 out=glmnet(x_new,y,alpha =1, lambda =0)
-lasso.coef=predict(out,newx=x_new)
-lasso.coef
+lasso.coef_1=predict(out,newx=x_new)
+lasso.coef_1
 
 
